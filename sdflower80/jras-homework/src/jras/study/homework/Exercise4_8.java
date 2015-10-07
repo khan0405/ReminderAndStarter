bac4a24c7f91ecbbd0bb271387cb5e43f65c99c6
sdflower80 <sdflower80@gmail.com>
2015-10-07 12:48:26 +0900

    과제 4-8

--- sdflower80/jras-homework/src/jras/study/homework/Exercise4_8.java
+++ sdflower80/jras-homework/src/jras/study/homework/Exercise4_8.java
@@ -1,15 +1,36 @@
 package jras.study.homework;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+
 public class Exercise4_8 {
 	public static void main(String[] args) {
 		new Exercise4_8().doHomework();
 	}
 	
+	private List<NextXy> answerList;
+	
+	public Exercise4_8() {
+		answerList = new ArrayList<>();
+	}
+	
 	public void doHomework() {
-		XyZeroBaseCoordinateIterator iterator = new XyZeroBaseCoordinateIterator(1, 10);
+		XyZeroBaseCoordinateIterator iterator = new XyZeroBaseCoordinateIterator(11, 11);
 		while(iterator.hasNext()) {
-			int[] nextXy = iterator.nextXy();
-			System.out.printf("x = %d, y = %d\n", nextXy[0], nextXy[1]);
+			calculate(iterator.nextXy(), (NextXy xy) -> answerList.add(xy));
+		}
+		
+		for(NextXy nextXy : answerList) {
+			System.out.printf("x = %d, y = %d\n", nextXy.x(), nextXy.y());
+		}
+	}
+	
+	protected void calculate(NextXy nextXy, Consumer<NextXy> c) {
+		if (((2*nextXy.x()) + (4*nextXy.y()) - 10) == 0) {
+			c.accept(nextXy);
 		}
 	}
 	
@@ -33,24 +54,38 @@ public class Exercise4_8 {
 			return false;
 		}
 		
-		public int[] nextXy() {
+		public NextXy nextXy() {
 			
-			int[] xy = new int[]{x, y};
+			NextXy xy = new NextXy(x, y);
 			
 			if (y <= maxY) {
 				y++;
 		 	}
 			
 			if (x <= maxX && y == maxY) {
-				x++;
 				y = 0;
-			}
-			
-			if (x == maxX) {
-				x = 0;
+				x++;
 			}
 			
 			return xy;
-		}		
+		}
+	}
+	
+	class NextXy {
+		private int x;
+		private int y;
+		
+		public NextXy(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+		
+		public int x() {
+			return x;
+		}
+		
+		public int y() {
+			return y;
+		}
 	}
 }
