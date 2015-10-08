601c6d93482f96159a47e81d76ce6a9c1b01f027
khan0405 <khan4257@gmail.com>
2015-10-09 02:24:01 +0900

    연습문제 6-3

---
+++ khan/sources/exercise/src/jras/study/exercise/Exercise6_3.java
@@ -0,0 +1,16 @@
+package jras.study.exercise;
+
+/**
+ * Created by KHAN on 2015-10-09.
+ */
+public class Exercise6_3 {
+
+	class Student {
+		String name;
+		int ban;
+		int no;
+		int kor;
+		int eng;
+		int math;
+	}
+}
