96077ed23a4fa03ae751387f05c53059167351fe
KimHunJin <ysg01129@naver.com>
2015-10-20 16:45:07 +0900

    10월 21일 과제 제출 - 시험 때문에 소스를 뜯어고치는건 못했네요..

--- ysg01129/jras-git-setting/Exam/src/example1014/Exam6_25.java
+++ ysg01129/jras-git-setting/Exam/src/example1014/Exam6_25.java
@@ -5,7 +5,6 @@ import java.util.HashSet;
 
 public class Exam6_25 {
 
-
 	private final int MAX_CARD_NUMBER = 13;
 
 	public static void main(String[] args) {
@@ -16,56 +15,58 @@ public class Exam6_25 {
 	private void ildanmandlgoboza() {
 		// TODO Auto-generated method stub
 
-		ArrayList<String> cardNumber = new ArrayList<>();		
+		ArrayList<String> cardNumber = new ArrayList<>();
 		ArrayList<String> cardPattern = new ArrayList<>();
-		
+
 		HashSet<String> hashSet = new HashSet<>();
 		ArrayList<String> arrDeduplicationList;
-		
+
 		cardPattern.add("HEART");
 		cardPattern.add("SPACE");
 		cardPattern.add("DIAMOND");
 		cardPattern.add("CLUB");
-		
-		for(int i=1;i<=MAX_CARD_NUMBER;i++) {
-			if(i==1) {
+
+		for (int i = 1; i <= MAX_CARD_NUMBER; i++) {
+			if (i == 1) {
 				cardNumber.add("ACE");
-			} else if(i==11) {
+			} else if (i == 11) {
 				cardNumber.add("JACK");
-			} else if(i==12) {
+			} else if (i == 12) {
 				cardNumber.add("QUEEN");
-			} else if(i==13) {
+			} else if (i == 13) {
 				cardNumber.add("KING");
 			} else {
-				cardNumber.add(i+"");
+				cardNumber.add(i + "");
 			}
 		}
-		
-		while(hashSet.size()!=3) {
-			int extnctionCardNumber = (int)(Math.random()*cardNumber.size());
-			int extenctionCardPattenr = (int)(Math.random()*cardPattern.size());
-			
+
+		while (hashSet.size() < 3) {
+			int extnctionCardNumber = (int) (Math.random() * cardNumber.size());
+			int extenctionCardPattenr = (int) (Math.random() * cardPattern.size());
+
 			hashSet.add(cardPattern.get(extenctionCardPattenr) + " " + cardNumber.get(extnctionCardNumber));
 		}
-		
+
 		arrDeduplicationList = new ArrayList<>(hashSet);
-		
-		String[] spiritText = null;
-		for(int i=0;i<arrDeduplicationList.size();i++) {
-			String text = arrDeduplicationList.get(i);
-			spiritText = text.split(" ");
-			System.out.println(spiritText[0] + " " + spiritText[1]);
+		for (String string : arrDeduplicationList) {
+			
+			System.out.println(string);
 		}
+		
+//		String[] spiritText = null;
+//		for (int i = 0; i < arrDeduplicationList.size(); i++) {
+//			String text = arrDeduplicationList.get(i);
+//			spiritText = text.split(" ");
+//			System.out.println(spiritText[0] + " " + spiritText[1]);
+//		}
 	}
 }
-
 
 // OOP 미완
 class Card {
 
-
 	private ArrayList<ArrayList<String>> card = new ArrayList<>();
-
+	
 
 	/**
 	 * 카드 생성
@@ -78,18 +79,19 @@ class Card {
 	 * 카드 모양 생성
 	 */
 	private void makeCardPattern() {
-		for(int i=0;i<card.size();i++) {
+		for (int i = 0; i < card.size(); i++) {
 			makeCardNumber(i);
 		}
 	}
 
 	/**
 	 * 카드 숫자 생성
+	 * 
 	 * @param i
 	 */
 	private void makeCardNumber(int i) {
-		for(int j=1;j<=13;j++) {			
-			card.get(i).add(j+"");
+		for (int j = 1; j <= 13; j++) {
+			card.get(i).add(j + "");
 		}
 	}
 }
--- ysg01129/jras-git-setting/Exam/src/example1014/Student.java
+++ ysg01129/jras-git-setting/Exam/src/example1014/Student.java
@@ -2,8 +2,7 @@ package example1014;
 
 /**
  * 
- * Student 클래스 입니다.
- * 값들을 얻어와 처리를 하도록 만들었습니다.
+ * Student 클래스 입니다. 값들을 얻어와 처리를 하도록 만들었습니다.
  * 
  * @author HunJin
  *
@@ -17,18 +16,19 @@ public class Student {
 	public int kor;
 	public int eng;
 	public int math;
-	private int total;	
+	private int total;
 	private float average;
-	
+
 	/**
 	 * 메서드 오버로딩을 하여 4번 문제와 5번 문제를 동시에 풀 수 있도록 하였습니다.
 	 */
 	Student() {
-		
+
 	}
-	
+
 	/**
 	 * 기본적으로 처음 실행이 되면 매개변수가 있을 경우 다음과 같은 메서드가 실행됩니다.
+	 * 
 	 * @param name
 	 * @param ban
 	 * @param no
@@ -37,31 +37,31 @@ public class Student {
 	 * @param math
 	 */
 	Student(String name, int ban, int no, int kor, int eng, int math) {
-		setName(name);  // 이름 입력
-		setBan(ban);  // 반 입력
-		setNo(no);  // 숫자 입력
-		setKor(kor);  // 한국어 점수 입력
-		setEng(eng);  // 영어 점수 입력
-		setMath(math);  // 수학 점수 입력
-		total(kor,eng,math);  // 총점 추출
-		average(total, 3);  // 평균 추출
-		
+		setName(name); // 이름 입력
+		setBan(ban); // 반 입력
+		setNo(no); // 숫자 입력
+		setKor(kor); // 한국어 점수 입력
+		setEng(eng); // 영어 점수 입력
+		setMath(math); // 수학 점수 입력
+		total(kor, eng, math); // 총점 추출
+		average(total, 3); // 평균 추출
+
 	}
-	
 
 	/**
-	 * 입력한 값을 1열로 뽑아내는 메서드 입니다.
-	 * 5번 문제와 연관되있습니다.
+	 * 입력한 값을 1열로 뽑아내는 메서드 입니다. 5번 문제와 연관되있습니다.
+	 * 
 	 * @return
 	 */
 	public String info() {
 
-		return getName() + "," + getBan() + "," + getNo() + "," + getKor() + "," + getEng() + "," + getMath() +"," + getTotal() + "," + getAverage();
+		return getName() + "," + getBan() + "," + getNo() + "," + getKor() + "," + getEng() + "," + getMath() + ","
+				+ getTotal() + "," + getAverage();
 
 	}
-	
+
 	// getter and setter
-	
+
 	public String getName() {
 		return name;
 	}
@@ -114,15 +114,16 @@ public class Student {
 		total = kor + math + eng;
 		return total;
 	}
-		
+
 	/**
 	 * 오버로딩 메서드 입니다.
+	 * 
 	 * @param kor
 	 * @param eng
 	 * @param math
 	 * @return
 	 */
-	public int getTotal(int kor, int eng, int math) {		
+	public int getTotal(int kor, int eng, int math) {
 		total(kor, eng, math);
 		return total;
 	}
@@ -130,49 +131,52 @@ public class Student {
 	public void setTotal(int total) {
 		this.total = total;
 	}
-	
+
 	public float getAverage() {
 		average(total, 3);
 		return average;
 	}
-	
+
 	/**
 	 * 마찬가지로 오버로딩 메서드 입니다.
+	 * 
 	 * @param total
 	 * @param subjectNumber
 	 * @return
 	 */
 	public float getAverage(int total, int subjectNumber) {
-		average = (float)total / subjectNumber;
+		average = (float) total / subjectNumber;
 		average = average * 10;
-		average = (float)Math.round(average) / 10;
+		average = (float) Math.round(average) / 10;
 		return average;
 	}
-	
+
 	public void setAverage(float average) {
 		this.average = average;
 	}
-	
+
 	/**
 	 * 총점을 구하는 메서드 입니다.
+	 * 
 	 * @param kor
 	 * @param eng
 	 * @param math
 	 */
-	public void total(int kor, int eng, int math) {		
-		total = kor + eng + math;		
+	public void total(int kor, int eng, int math) {
+		total = kor + eng + math;
 		setTotal(total);
 	}
-	
+
 	/**
 	 * 소수점 두번째 자리에서 반올림 하여 평균을 구하는 메서드 입니다.
+	 * 
 	 * @param total
 	 * @param subjectNumber
 	 */
 	public void average(int total, int subjectNumber) {
-		average = (float)total / subjectNumber;
+		average = (float) total / subjectNumber;
 		average = average * 10;
-		average = (float)Math.round(average) / 10;
+		average = (float) Math.round(average) / 10;
 		setAverage(average);
 	}
 }
